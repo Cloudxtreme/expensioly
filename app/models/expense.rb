b3145a44deb1c611b5f645037953ce086aec9801
Harry Schwartz <hello@harryrschwartz.com>
2015-10-26 23:56:09 -0400

    Ensure that expense amounts are positive

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,6 +3,7 @@ class Expense < ActiveRecord::Base
   belongs_to :user
 
   validates :amount, presence: true
+  validates :amount, numericality: { greater_than_or_equal_to: 0 }
   validates :category, presence: true
   validates :name, presence: true
 
